2189c1c62c272becb7490997e0f7fad8e067d4bd
Abisoye Durojaiye <abisoyeodurojaiye@gmail.com>
2022-02-14 04:20:45 +0100

    module 1

--- src/reminder.py
+++ src/reminder.py
@@ -5,3 +5,13 @@ class PrefixedReminder:
     def __init__(self, prefix="Hey, don't forget to "):
         self.prefix = prefix
         self.text = prefix + '<placeholder_text>'
+
+class PoliteReminder(PrefixedReminder):
+    """[summary]
+
+    Args:
+        PrefixedReminder ([type]): [description]
+    """
+    def __init__(self, text, prefix="Hey, don't forget to "):
+        text = prefix + text
+        super().__init__(prefix='Please do =>')
